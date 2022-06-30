d659bbbbfdbb970134218510351d22d5128fc506
dirk.barchmann <dirk.barchmann@zeiss.com>
2022-06-30 22:15:34 +0200

    Initial Commit

---
+++ IceAndFire/IceAndFireApp.swift
@@ -0,0 +1,17 @@
+//
+//  IceAndFireApp.swift
+//  IceAndFire
+//
+//  Created by DIDBARCH on 30.06.22.
+//
+
+import SwiftUI
+
+@main
+struct IceAndFireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
